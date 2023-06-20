2526fe636378c3a0f387a4f8f473c7260247e47b
phucnguyen1605 <54256779+phucnguyen1605@users.noreply.github.com>
2023-06-20 17:42:30 +0700

    Revert "Init check"

--- src/service/serviceimpl/user_service_impl_test.go
+++ src/service/serviceimpl/user_service_impl_test.go
@@ -74,7 +74,7 @@ func (s *UserServiceTestSuite) TestCreateUser_ShouldReturnServerError() {
 
 	s.userRepository.
 		On("CreateUser", user2).
-		Return(nil, nil)
+		Return(nil, ErrUnexpected)
 
 	userService := NewUserService(s.userRepository, s.token, s.password)
 
